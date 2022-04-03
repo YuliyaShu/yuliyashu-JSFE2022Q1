f292f175883c05057206ab048a35f0cc8612835b
YuliyaShu <juliette7373@gmail.com>
2022-04-03 15:51:09 +0200

    docs: add pets.json

---
+++ shelter/pages/pets/script.js
@@ -0,0 +1,12 @@
+// const sliderPets = require('./pets.json'); 
+// // console.log(sliderPets);
+// const sliderPets = JSON.parse('./pets.json');
+// console.log()
+
+// const sliderImg = document.createElement('img');
+// sliderImg.style.content(URL )
+const sliderPets = require('./pets.json');
+console.log(sliderPets[0].name);
+
+const slider = document.querySelector('.slider');
+console.log(slider)
