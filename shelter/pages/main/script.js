44f812bf8912ed02843060fede946483956c3d43
YuliyaShu <juliette7373@gmail.com>
2022-04-04 11:39:05 +0200

    fix: links corrections

--- shelter/pages/main/script.js
+++ shelter/pages/main/script.js
@@ -8,5 +8,10 @@ function buttonToOurFriends() {
 
 
 function buttonToOurFriendsPage() {
-    window.location.href="https://rolling-scopes-school.github.io/yuliyashu-JSFE2022Q1/shelter/pages/pets/index.html";
+    window.open('https://rolling-scopes-school.github.io/yuliyashu-JSFE2022Q1/shelter/pages/pets/index.html', '_blank');
+    
+}
+
+function inAdditionButton() {
+    window.open('#', '_blank');
 }
