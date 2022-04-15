fea8a15b2a48622bd0a0fdf4a8c69356512b3bb1
YuliyaShu <juliette7373@gmail.com>
2022-04-15 08:53:17 +0200

    feat: adaptive, popup, burger

--- shelter/pages/main/script.js
+++ shelter/pages/main/script.js
@@ -15,3 +15,46 @@ function buttonToOurFriendsPage() {
 function inAdditionButton() {
     window.open('#', '_blank');
 }
+
+
+
+
+
+
+//burger-menu
+
+const burger = document.querySelector('.nav-header-burger');
+const burgerUl = document.querySelector('ul');
+const navBurgerMenu = document.querySelector('.nav-burger-menu');
+const navBurgerMenuContainer = document.querySelector('.nav-burger-menu-container');
+const navBurgerMenuList = document.querySelector('.nav-burger-menu-list');
+const navBurgerMenuLink1 = document.querySelector('.nav-burger-menu-link-1');
+const navBurgerMenuLink2 = document.querySelector('.nav-burger-menu-link-2');
+const navBurgerMenuLink3 = document.querySelector('.nav-burger-menu-link-3');
+const navBurgerMenuLink4 = document.querySelector('.nav-burger-menu-link-4');
+const logoBurger = document.querySelector('.logo-burger');
+const bodyBurger = document.querySelector('.body-burger');
+
+function toggleMenu() {
+  bodyBurger.classList.toggle('overlay');
+  logoBurger.classList.toggle('open');
+  burgerUl.classList.toggle('open');
+  navBurgerMenu.classList.toggle('open');
+  navBurgerMenuContainer.classList.toggle('open');
+  navBurgerMenuList.classList.toggle('open');
+  navBurgerMenuLink1.classList.toggle('open');
+  navBurgerMenuLink2.classList.toggle('open');
+  navBurgerMenuLink3.classList.toggle('open');
+  navBurgerMenuLink4.classList.toggle('open');
+  burger.classList.toggle('open');
+}
+
+burger.addEventListener('click', toggleMenu);
+navBurgerMenuLink1.addEventListener('click', toggleMenu);
+navBurgerMenuLink2.addEventListener('click', toggleMenu);
+navBurgerMenuLink3.addEventListener('click', toggleMenu);
+navBurgerMenuLink4.addEventListener('click', toggleMenu);
+bodyBurger.addEventListener('click', toggleMenu);
+
+
+
