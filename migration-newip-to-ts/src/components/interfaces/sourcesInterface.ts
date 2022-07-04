5a68d0aebd71d70e3b1bca073d9cdb895dc0e2ff
YuliyaShu <juliette7373@gmail.com>
2022-07-04 12:03:37 +0200

    feat: add enum

--- migration-newip-to-ts/src/components/interfaces/sourcesInterface.ts
+++ migration-newip-to-ts/src/components/interfaces/sourcesInterface.ts
@@ -12,9 +12,17 @@ interface SourcesSources extends Array<SourcesUnit> {
     [index: number]: SourcesUnit;
 }
 
+// eslint-disable-next-line no-unused-vars
+enum Status {
+    // eslint-disable-next-line no-unused-vars
+    ok = 'ok',
+    // eslint-disable-next-line no-unused-vars
+    error = 'error',
+}
+
 interface SourceInt extends Array<SourcesSources> {
     sources: SourcesSources;
-    status: string;
+    status: Status.ok;
 }
 
 export { SourcesUnit, SourceInt, SourcesSources };
