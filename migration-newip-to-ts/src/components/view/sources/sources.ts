c68e559493acf3ffe58e1d11410ecbd577e75349
YuliyaShu <juliette7373@gmail.com>
2022-07-07 14:00:19 +0200

    refactor: avoid using "as", sources.ts

--- migration-newip-to-ts/src/components/view/sources/sources.ts
+++ migration-newip-to-ts/src/components/view/sources/sources.ts
@@ -4,21 +4,22 @@ import { SourcesUnit, SourcesSources } from '../../interfaces/sourcesInterface';
 class Sources {
     draw(data: SourcesSources): void {
         const fragment: DocumentFragment = document.createDocumentFragment();
-        const sourceItemTemp = document.querySelector('#sourceItemTemp') as HTMLTemplateElement;
+        const sourceItemTemp = document.querySelector('#sourceItemTemp');
 
-        data.forEach((item: SourcesUnit) => {
-            const sourceClone = sourceItemTemp?.content.cloneNode(true) as DocumentFragment;
-
-            const sourceCloneElement: HTMLElement | null = sourceClone?.querySelector('.source__item-name');
-            if (sourceCloneElement) {
-                sourceCloneElement.textContent = item.name;
-                sourceCloneElement.setAttribute('data-source-id', item.id);
-            }
-
-            fragment.append(sourceClone);
-        });
-
-        document.querySelector<HTMLElement>('.sources')?.append(fragment);
+        if (sourceItemTemp instanceof HTMLTemplateElement) {
+            data.forEach((item: SourcesUnit) => {
+                const sourceClone = sourceItemTemp?.content.cloneNode(true);
+                if (sourceClone instanceof DocumentFragment) {
+                    const sourceCloneElement: HTMLElement | null = sourceClone?.querySelector('.source__item-name');
+                    if (sourceCloneElement) {
+                        sourceCloneElement.textContent = item.name;
+                        sourceCloneElement.setAttribute('data-source-id', item.id);
+                    }
+                    fragment.append(sourceClone);
+                }
+            });
+            document.querySelector<HTMLElement>('.sources')?.append(fragment);
+        }
     }
 }
 
