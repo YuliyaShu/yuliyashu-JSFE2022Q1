7a8465bff8eed2e8b9f6fa9331cfa60cb433e91c
YuliyaShu <juliette7373@gmail.com>
2022-07-07 13:54:32 +0200

    refactor: correct optional data.articles, appView.ts

--- migration-newip-to-ts/src/components/view/appView.ts
+++ migration-newip-to-ts/src/components/view/appView.ts
@@ -12,12 +12,12 @@ class AppView {
     }
 
     drawNews(data?: NewsInt): void {
-        const values: NewsUnit[] = data?.articles ? data?.articles : [];
+        const values: NewsUnit[] = data?.articles ? data.articles : [];
         this.news.draw(values as NewsUnit[]);
     }
 
     drawSources(data?: SourceInt): void {
-        const values: SourcesUnit[] = data?.sources ? data?.sources : [];
+        const values: SourcesUnit[] = data?.sources ? data.sources : [];
         this.sources.draw(values as SourcesUnit[]);
     }
 }
