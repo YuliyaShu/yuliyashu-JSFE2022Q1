752b05613437fbcfcec8cb0c1328a0eed99955b8
YuliyaShu <juliette7373@gmail.com>
2022-07-19 10:51:14 +0200

    fix: delete comments from index.ts

--- online-store/src/index.ts
+++ online-store/src/index.ts
@@ -2,12 +2,8 @@ import Posters from './assets/scripts/components/Posters';
 import ShopView from './assets/scripts/view/ShopView';
 import './style.css';
 
-// try {
 const shop: ShopView = new ShopView();
 shop.start();
-// } catch(e) {
-//   // load image 404
-// c
 Posters.setCategoriesData();
 
 console.log('Информация для cross-check проверяющего: все пункты ТЗ Online store выполнены.')
