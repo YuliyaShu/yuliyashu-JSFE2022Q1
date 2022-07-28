34007feb4024bc4609e549f09254ff13bc550cb2
YuliyaShu <juliette7373@gmail.com>
2022-07-28 12:12:42 +0200

    fix: correct test cases

--- online-store/src/scripts/__tests__/AnyElement-constructor.test.ts
+++ online-store/src/scripts/__tests__/AnyElement-constructor.test.ts
@@ -3,19 +3,19 @@ import AnyElement from "../elements/AnyElement";
 describe("AnyElement constructor", () => {
 
   test('Parameter parent is correct', () => {
-    const container = document.querySelector('.container') as HTMLElement;
+    const container = document.createElement('div');
     const newElement = new AnyElement(container, { type: 'div', className: ['header__search'] });
     expect(newElement.parent).toBe(container);
   });
 
   test('Parameter options type is correct', () => {
-    const container = document.querySelector('.container') as HTMLElement;
+    const container = document.createElement('div');
     const newElement = new AnyElement(container, { type: 'div', className: ['header__search'] });
     expect(newElement.options.type).toBe('div');
   });
 
   test('Parameter options className is correct', () => {
-    const container = document.querySelector('.container') as HTMLElement;
+    const container = document.createElement('div');
     const newElement = new AnyElement(container, { type: 'div', className: ['header__search'] });
     expect(newElement.options.className).toStrictEqual(['header__search']);
   })
